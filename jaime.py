86e77dafd7222fc7a3665a1d27b432147a4b8b17
Jaime Negrete <jrnp1997@gmail.com>
2018-05-22 11:21:24 -0500

    Add pprint module to better display information

--- jaime.py
+++ jaime.py
@@ -1,7 +1,8 @@
-n = 5
+from pprint import pprint
+n = 20
 #Inicializando la serie
 ser = [i if i<=1 else 0 for i in range(n)]
 # Calculando la serie fibonnaci
 for i in range(2, n):
  	ser[i] = ser[i-1] + ser[i-2]
-print(ser)
+pprint(ser)
