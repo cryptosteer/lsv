62b69519745ce1dfef9634d2f9c36f74c1a24c89
Oscles <osnaiderluis94@hotmail.com>
2018-05-22 11:23:45 -0500

    funcion fibonacci super veloz

--- oscles.py
+++ oscles.py
@@ -1,5 +1,17 @@
-de main():
-    pass
+from functools import lru_cache
+
+
+@lru_cache(maxsize=10)
+def fibonacci(number):
+    if number < 2:
+        return number
+    else:
+        return fibonacci(number - 1) + fibonacci(number - 2)
+
+
+def main():
+    print([fibonacci(number) for number in range(2000)])
+
 
 if __name__ == '__main__':
     main()
