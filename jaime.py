64b5d5dd4a894ccc5d8c6dbeef984cecc7b37cc2
Jaime Negrete <jrnp1997@gmail.com>
2018-05-22 11:33:23 -0500

    Add timer to view execution time

--- jaime.py
+++ jaime.py
@@ -1,8 +1,12 @@
 from pprint import pprint
-n = 20
+import time
+n = 4000
+start = time.perf_counter()
 #Inicializando la serie
 ser = [i if i<=1 else 0 for i in range(n)]
 # Calculando la serie fibonnaci
 for i in range(2, n):
  	ser[i] = ser[i-1] + ser[i-2]
+end = time.perf_counter()
 pprint(ser)
+print("="*30+"\n","Calculada en {} segundos".format(end-start))
