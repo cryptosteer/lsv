581841c44d8bf9694de3e809bfd94c17451c0bd4
Jaime Negrete <jrnp1997@gmail.com>
2018-05-22 11:15:54 -0500

    Fibonacci

--- jaime.py
+++ jaime.py
@@ -1,1 +1,7 @@
-print("Esto es una prueba")
+n = 5
+#Inicializando la serie
+ser = [i if i<=1 else 0 for i in range(n)]
+# Calculando la serie fibonnaci
+for i in range(2, n):
+ 	ser[i] = ser[i-1] + ser[i-2]
+print(ser)
