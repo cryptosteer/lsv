46e07fdc2888a6ed13907f722afb86746a5ce3a3
Oscles <osnaiderluis94@hotmail.com>
2018-05-23 09:21:02 -0500

    Subiendo mi codigo

--- 01.py
+++ 01.py
@@ -1,4 +1,7 @@
 """
-Imprimir en una linea separados por coma todos los elementos que sean divisibles por 7 y no sean multipolos de 5 entre 20000 y 32000 (Incluyendo estos elementos).
-Trabajar con listas.
+Imprimir en una linea separados por coma todos los elementos que sean divisibles por 7 y no sean multipolos de 5 entre 20000 y 32000
+(Incluyendo estos elementos).Trabajar con listas.
 """
+
+print([number for number in range(20000, 32000) if number % 7 == 0 and not number % 5 == 0])
+
