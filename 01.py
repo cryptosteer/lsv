869f62fbcea24c887aabcb0398c187e1278741c7
pedrocastilla <castillabend@gmail.com>
2018-05-23 10:03:43 -0500

    ejercicio 01

--- 01.py
+++ 01.py
@@ -2,13 +2,15 @@
 
 c = 0
 l1 = [7, 5, 14, 35]
-l2 = []
-for i in l1:
+
+def lista():
+
+for i in range(20000,32000):
     if i % 7 == 0 and i %5 != 0:
 
-        l2[c] = i
-        c = c+1
-        print(l2)
+        lista.add = [i]
+
+        print (lista())
     else: print("No existen numeros que cumplan con lo requerido")
 
 
@@ -19,3 +21,8 @@ Imprimir en una linea separados por coma todos los elementos que sean divisibles
 Trabajar con listas.
 
 """
+
+l2 = []
+c = c+1
+
+print (l2)
