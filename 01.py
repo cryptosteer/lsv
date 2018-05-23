5f42fbfabd9b8c11f54b61be92a147b036b2d56a
John Florez <johnflorez_1289@hotmail.com>
2018-05-23 09:26:31 -0500

    Ejercicio Lista

--- 01.py
+++ 01.py
@@ -1,4 +1,13 @@
 """
-Imprimir en una linea separados por coma todos los elementos que sean divisibles por 7 y no sean multipolos de 5 entre 20000 y 32000 (Incluyendo estos elementos).
+Imprimir en una linea separados por coma todos los elementos que sean divisibles por 7 y no sean multiplos de 5 entre 20000 y 32000 (Incluyendo estos elementos).
 Trabajar con listas.
 """
+lista = []
+for i in range(20000, 32001):
+	if i%7==0 and i%5!=0:
+		lista.append(i)
+
+print("Elementos divisibles por 7 y no multiplos de 5 entre 20000 y 32000")
+for i in lista:
+	print(i, end=",") 
+
