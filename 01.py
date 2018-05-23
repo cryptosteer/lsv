1d824c9956719dfa9519b29788a9bc1e293cedce
rafael barboza <barboza.rafael.p@gmail.com>
2018-05-23 10:17:27 -0500

    funciones

--- 01.py
+++ 01.py
@@ -2,3 +2,12 @@
 Imprimir en una linea separados por coma todos los elementos que sean divisibles por 7 y no sean multipolos de 5 entre 20000 y 32000 (Incluyendo estos elementos).
 Trabajar con listas.
 """
+def imprimir():
+    numero=[]
+    for i in range(20000,32000):
+        if (i%7==0 and i%5!=0) :
+            numero.append(i)
+    print (numero)
+
+
+imprimir()
