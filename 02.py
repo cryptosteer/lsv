10213a69b5920b76696f451df1d7d2f8659cc279
pedrocastilla <castillabend@gmail.com>
2018-05-23 10:53:57 -0500

    ejercicio 02

--- 02.py
+++ 02.py
@@ -19,4 +19,27 @@ off to its nearest value (for example, if the output received is 26.0,
 it should be printed as 26)
 In case of input data being supplied to the question, it should be assumed
 to be a console input.
-""" 
+"""
+
+import math
+def calcular():
+    C = 50
+    H = 30
+    entradas = [100 ,150 ,180]
+    for i in entradas:
+        r = math.sqrt((2 * C * i) / H)
+
+        print( int(r))
+
+
+calcular()
+
+
+
+
+
+
+
+
+
+
