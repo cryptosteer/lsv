5f31a28f53252bcb61b03a8853f7b1f3431d5e5a
pedrocastilla <castillabend@gmail.com>
2018-05-23 10:21:29 -0500

    ejercicio 01

--- 01.py
+++ 01.py
@@ -1,29 +1,24 @@
 """PEDRO"""
 
-c = 0
-l1 = [7, 5, 14, 35]
-
-def lista():
-
-for i in range(20000,32000):
-    if i % 7 == 0 and i %5 != 0:
-
-        lista.append = [i]
-        print (lista())
-
-
-
-
-
 """
 Imprimir en una linea separados por coma todos los elementos que sean divisibles por 7 y no sean multipolos de 5 entre 20000 y 32000 (Incluyendo estos elementos).
 Trabajar con listas.
 
 """
 
-l2 = []
-c = c+1
 
-print (l2)
 
-else: print("No existen numeros que cumplan con lo requerido")
+def lista():
+    lista2 = []
+
+    for i in range(20000,32000):
+        if i % 7 == 0 and i %5 != 0:
+
+            lista2.append(i)
+    print (lista2)
+
+lista()
+
+
+
+
