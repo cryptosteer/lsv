2c24d9a2b94ed9076eabe16b9e93c7b22bba1b0a
Daniel Nuñez <danielsistem.ing96@gmail.com>
2018-05-23 09:04:25 -0500

    Ejercicio 01

--- 01.py
+++ 01.py
@@ -2,3 +2,11 @@
 Imprimir en una linea separados por coma todos los elementos que sean divisibles por 7 y no sean multipolos de 5 entre 20000 y 32000 (Incluyendo estos elementos).
 Trabajar con listas.
 """
+def listar():
+    lista=[]
+    for i in range(20000,32001):
+        if (i%7)==0 and (i%5)!=0:
+            lista.append(i)
+    return lista
+
+print(listar())
