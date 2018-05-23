ef97e1a9b53858e65a012a9e980ab05131c54bfa
John Florez <johnflorez_1289@hotmail.com>
2018-05-23 09:41:35 -0500

    Prueba

--- 01.py
+++ 01.py
@@ -1,4 +1,5 @@
 """
 Imprimir en una linea separados por coma todos los elementos que sean divisibles por 7 y no sean multipolos de 5 entre 20000 y 32000 (Incluyendo estos elementos).
 Trabajar con listas.
+
 """
