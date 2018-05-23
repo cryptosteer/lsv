bb7dc149a9533421e3fad04c8054ecaadaf1a158
Oscles <osnaiderluis94@hotmail.com>
2018-05-23 10:02:58 -0500

    Subiendo mi codigo con pprint

--- 01.py
+++ 01.py
@@ -2,6 +2,10 @@
 Imprimir en una linea separados por coma todos los elementos que sean divisibles por 7 y no sean multipolos de 5 entre 20000 y 32000
 (Incluyendo estos elementos).Trabajar con listas.
 """
+from pprint import pprint
 
-print([number for number in range(20000, 32000) if number % 7 == 0 and not number % 5 == 0])
-
+pprint(
+    [number for number in range(20000, 32000) if number % 7 == 0 and not number % 5 == 0],
+    indent=3,
+    compact=True
+)
