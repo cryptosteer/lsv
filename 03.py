f80857135f80a553d1b1fdb9b9d24a8613b1b5d0
John Florez <johnflorez_1289@hotmail.com>
2018-05-24 12:35:51 -0500

    Ejercicio clases

--- 03.py
+++ 03.py
@@ -3,19 +3,60 @@ Escribir una clase en python llamada circulo que contenga un radio, con un méto
 """
 from math import pi
 
-class Circulo:
-	def __init__(self, radio):
-		self.radio = radio
+class Figura:
+	
+	figura = "Ninguno"
+
+	def descripcion(self):
+		return "Soy un {}".format(self.figura)
 
 	def area(self):
-		area = pi * (self.radio**2)
-		print("El área del circulo es: ", area)
-
+		print("El area de una figura")
+		
 	def perimetro(self):
-		perimetro = 2*pi*self.radio
-		print("El perimetro del circulo es: ", perimetro)
+		print("El perimetro de una figura")
+
+	def __str__(self):
+		msg = self.descripcion() + "\n"
+		msg += "El área es: {}\n".format(self.area())
+		msg += "El perimetro es: {}\n".format(self.perimetro())
+		msg += "---------------------------------------------"
+		return msg
+		
+	
+class Circulo(Figura):
+	def __init__(self, radio):
+		self.radio = radio
+		self.figura = "Circulo"
+
+	def area(self):
+		return pi * (self.radio**2)
+		
+	def perimetro(self):
+		return 2*pi*self.radio
+		
+	def __str__(self):
+		return super(Circulo, self).__str__()
+		
+
+class Cuadrado(Figura):
+	def __init__(self, lado):
+		self.lado = lado
+		self.figura = "Cuadrado"
+
+	def area(self):
+		return self.lado**2
+		
+	def perimetro(self):
+		return self.lado * 4
+		
+	def __str__(self):
+		return super(Cuadrado, self).__str__() 
 
 radio = int(input("Digite el radio del circulo: "))
-c = Circulo(radio)
-c.area()
-c.perimetro()
+circulo = Circulo(radio)
+print(circulo)
+
+lado = int(input("Digite el lado del cuadrado: "))
+cuadrado = Cuadrado(lado)
+print(cuadrado)
