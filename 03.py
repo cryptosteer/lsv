a649e428a30e63df68ca93fdca7dfa866e9b2106
Daniel Nuñez <danielsistem.ing96@gmail.com>
2018-05-24 10:33:23 -0500

    Ejercicio 03 - Se añadio la clase Figura y la clase Cuadrado; Se añadio la herencia de Figura en cuadrado y circulo

--- 03.py
+++ 03.py
@@ -2,13 +2,28 @@
 Escribir una clase en python llamada circulo que contenga un radio, con un método que devuelva el área y otro que devuelva el perímetro del circulo.
 """
 import math
-class Circulo:
+class Figura:
+    def descripcion(self):
+        print("Soy una figura")
+class Circulo(Figura):
     def __init__(self,radio):
         self.radio=radio
     def area(self):
         return math.pi*(self.radio**2)
     def perimetro(self):
         return 2*math.pi*self.radio
-c=Circulo(int(input("Ingrese el radio deseado: ")))
-print("El area del circulo es: ", c.area())
-print("El perimetro del circulo es: ", c.perimetro())
+class Cuadrado(Figura):
+    def __init__(self,lado):
+        self.lado=lado
+    def area(self):
+        return self.lado*self.lado
+    def perimetro(self):
+        return self.lado*4
+#c=Circulo(int(input("Ingrese el radio deseado: ")))
+circulo=Circulo(8)
+print("El area del circulo es: ", circulo.area())
+print("El perimetro del circulo es: ", circulo.perimetro())
+circulo.descripcion()
+cuadrado=Cuadrado(2)
+print("El area del cuadrado es: ", cuadrado.area())
+print("El perimetro del cuadrado es: ", cuadrado.perimetro())
