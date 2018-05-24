db5932e57ef7bc4ba55163852d3e9376628621f1
Jaime Negrete <jrnp1997@gmail.com>
2018-05-24 10:44:45 -0500

    Add herence to exercise 3

--- 03.py
+++ 03.py
@@ -3,21 +3,64 @@ Escribir una clase en python llamada circulo que contenga un radio, con un méto
 """
 from math import pi
 
-class Circulo:
+class Figura:
+
+    name = "Ninguna"
+
+    def descripcion(self):
+        return "Soy una figura"
+
+    def area(self) -> float:
+        pass
+
+    def perimetro(self) -> float:
+        pass
+
+    def show(self):
+        print("="*20)
+        print("Caracteristicas del {}".format(self.name))
+        print("\tArea -> {}".format(self.area()))
+        print("\tPerimetro -> {}".format(self.perimetro()))
+        print("\tDescripcion -> {}".format(self.descripcion()))
+
+class Circulo(Figura):
 
     def __init__(self, rd):
         self.radio = rd
+        self.name = "Circulo"
 
-    def Area(self):
-        return pi*(self.radio**2)
+    def area(self):
+        return pi * (self.radio ** 2)
 
-    def Perimetro(self):
+    def perimetro(self):
         return 2*pi*self.radio
+
+    def show(self):
+        super().show()
+        print("\tRadio -> {}".format(self.radio))
+
+
+class Cuadrado(Figura):
+
+    def __init__(self, l):
+        self.lado_dim = l
+        self.name = "Cuadrado"
+
+    def area(self):
+        return self.lado_dim ** 2
+
+    def perimetro(self):
+        return 4 * self.lado_dim
+
+    def show(self):
+        super().show()
+        print("\tDimension de los lados -> {}".format(self.lado_dim))
 
 
 if __name__ == "__main__":
     my_circle = Circulo(4.90)
-    print("Caracteristicas del circulo")
-    print("\tRadio -> {}".format(my_circle.radio))
-    print("\tArea -> {}".format(my_circle.Area()))
-    print("\tPerimetro -> {}".format(my_circle.Perimetro()))
+    my_circle.show()
+
+    my_square = Cuadrado(4)
+    my_square.show()
+
