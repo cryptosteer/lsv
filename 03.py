c1a558292e9a80bd85c3e2a4e7f46579f8328424
andres benavides <andres_felipe16@hotmail.es>
2018-05-24 10:37:51 -0500

    Exercise

--- 03.py
+++ 03.py
@@ -4,7 +4,12 @@ Escribir una clase en python llamada circulo que contenga un radio, con un méto
 import math
 
 
-class Circulo:
+class Figure:
+    def Description(self):
+        return "I am a figure"
+
+
+class Circle(Figure):
 
     def __init__(self, radio):
         self.radio = radio
@@ -12,13 +17,38 @@ class Circulo:
     def cal_area(self):
         return math.pi * (self.radio ** 2)
 
-    def cal_permiter(self):
+    def cal_perimeter(self):
         return 2 * math.pi * self.radio
 
+    def Description(self):
+        return "I am a Circle"
+
+    def __str__(self):
+        return "Circle have a Area: {}, Perimeter: {}".format(self.cal_area(), self.cal_perimeter())
+
+
+class Squeare(Figure):
+
+    def __init__(self, side):
+        self.side = side
+
+    def cal_area(self):
+        return self.side ** 2
+
+    def cal_perimeter(self):
+        return 4 * self.side
+
+    def Description(self):
+        return "I am a Squeare"
+
+    def __str__(self):
+        return "Asqueare have a Area: {}, Perimeter: {}".format(self.cal_area(),self.cal_perimeter())
 
 if __name__ == '__main__':
 
-    A = Circulo()
+    A = Circle(2)
+    print(A)
+    print("------------------------------------------")
+    B = Squeare(2)
+    print(B)
 
-    print("The area is :", A.calarea())
-    print("THe perimeter is:", A.calpermiter())
