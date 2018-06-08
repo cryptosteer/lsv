1575cf54374648d680f528485743f2b0c4523a66
rafael barboza <barboza.rafael.p@gmail.com>
2018-06-08 10:01:54 -0500

    primero

--- E1.py
+++ E1.py
@@ -18,12 +18,11 @@ def popular(l):
     """Utilizamos el metodo most_common que Devuelve una lista de los n elementos más comunes y sus recuentos,
      del más comun."""
     popular=contar.most_common(1)[0][0]
-    ##return Counter(l).most_common(1)[0][0]
     return popular
 
 lista = [1, 3, 5, 6, 3, 6, 7, 8, 9, 6]
-##print("{} is the most popular.".format(popular(lista)))
-print(popular(lista))
+print("{} is the most popular.".format(popular(lista)))
+
 
 
 
