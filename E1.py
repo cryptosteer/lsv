f87077daebe8be92f4677b832f71c63e05849f19
dgun17 <t00038310@utbvirtual.edu.co>
2018-06-08 09:24:02 -0500

    Add comments

--- E1.py
+++ E1.py
@@ -10,14 +10,27 @@ Ej: [1, 3, 5, 6, 3, 6, 7, 8, 9, 6] => 6 (six is the most popular number)
 """
 
 def popular(l: list) -> int:
-    values = list(set(l))
+    """
+    This function get the most popular number on a list of integer numbers.
+    :param l: list of integer.
+    :return: python integer with the most popular number in the list.
+    """
+    values = list(set(l))  # Get list orderned ascendent without repeteat values
     count = []
-    for val in values:
-        count.append(l.count(val))
+    for val in values:  # Run over all integer values
+        num_repeat = l.count(val)  # Get repeat number of values on list.
+        count.append(num_repeat)  # Add repeat number to list
+    maximun = max(count)  # Get the first maximum numbers repeats
+    idx = count.index(maximun)  # Set index of number most popular.
+    """
+    Note: How the list is on ascendent order the first maximum number is the
+    most small
+    """
+    return values[idx]  # Return number
 
-    idx = count.index(max(count))
-    return values[idx]
-
-
-lista = [1, 3, 5, 6, 3, 6, 7, 8, 9, 6]
-print("{} is the most popular.".format(popular(lista)))
+if __name__ == "__main__":
+    listas = [[5, 5, 5, 4, 4, 3, 3, 3],
+              [1, 3, 5, 6, 3, 6, 7, 8, 9, 6]
+              ]
+    for lista in listas:
+        print("{} is the most popular.".format(popular(lista)))
--- E2.py
+++ E2.py
@@ -12,18 +12,29 @@ Casi-palindromo con aquellas cadenas que por un caracter pueden ser palindromo:
         "Isaac ni ronca así" ---> Casi-palindromo
 """
 
-def is_palindromo(frase: str) -> bool:
+def is_palindromo(frase: str) -> str:
+    """
+    Funcion que retorna el estado de palindromidad de una frase.
+    :param frase: string python (str) con la frase a verificar.
+    :return: mensaje de estado de la frase.
+    """
+    # Convirtiendo frase a lista excluyendo espacios y comas, y su contenido
+    # en minusculas, resultando una lista con los caracteres de la frase.
     normal = [letter.lower() for letter in list(frase) if letter != ' ' and letter != ',']
-    reverse = normal.copy()
-    reverse.reverse()
+    reverse = normal.copy()  # Creando una copia de la list de caracteres
+    reverse.reverse()  # Reversiando su contenido (caracteres en orden inverso)
+    #  Verificando si los caracteres en orden normal (A) se parecen a los
+    # de orden inverso (B), si es asi se establece 1 de lo contrario 0
     a = [1 if (A == B) else 0 for A, B in zip(normal, reverse)]
-    if all(a):
+    if all(a):  # Verificando si todos los caracteres dan resultados positivos
         return "Es palindromo"
     elif a.count(0) == 2:
+         # Si solo 1 caracter no compagina se dan 2 estados negativos.
         return "Es casi palindromo"
     else:
+        # Si no se cumplen ningunas de las anteriores no es palindromo
         return "No es palindromo"
-    # Complete
+
 
 if __name__ == "__main__":
     frases = ["Yo dono rosas, oro no doy", "Isaac ni ronca asi", "Hola"]
