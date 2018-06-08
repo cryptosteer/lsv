0ae8b94a017fe38e26c539033301db555a67ad34
joseph <pazgenes@hotmail.com>
2018-06-08 10:33:06 -0500

    terminado

--- E2.py
+++ E2.py
@@ -1,18 +1,25 @@
 """
 Dada una cadena de caracteres determine si dicha cadena:
-
     1. Es palindromo
     2. Es casi-palindromo
     3. No es palindromo
-
 Casi-palindromo con aquellas cadenas que por un caracter pueden ser palindromo:
-
     Ej: 
         "Yo dono rosas, oro no doy" ---> Palindromo
         "Isaac ni ronca así" ---> Casi-palindromo
 """
+a = 'Yo dono rosas, oro no doy'
+b = 'Isaac ni ronca así'
+def is_palindromo(x):
 
-def is_palindromo():
-    # Complete
+    a = x[::-1]
+    a = a.lower()
+    a = a.strip()
 
-print("{} ---> {}".format(frase, is_palindromo(frase)))
+    if x==a:
+        print(x," (es palindromo)")
+    else:
+        print(x," (no es palindromo)")
+
+
+is_palindromo(a)
